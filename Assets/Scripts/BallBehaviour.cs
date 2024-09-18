40fba7981b9c11ae4f0a72f9ae79f90aac2070b2
DallinEPIC <160791928+DallinEPIC@users.noreply.github.com>
2024-09-18 12:52:22 -0600

    Added collision detection
    + Ball bounces off paddle and edge of screen

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -4,16 +4,46 @@ using UnityEngine;
 
 public class BallBehaviour : MonoBehaviour
 {
-    private Vector3 _velocity;
-    public float Speed;
+    private Vector3 Velocity;
+    private PaddleController _paddle;
+    [SerializeField] public float _maxHorizontalSpeed;
+
+    private Vector3 _paddleSize, _paddlePos, _ballSize, _ballPos;
+
     void Start()
     {
-        _velocity = Vector3.down;
-        Speed = 10;
+        Velocity = Vector3.down;
     }
 
     void Update()
     {
-        transform.position += _velocity * Speed * Time.deltaTime;
+        _paddleSize = PaddleController.Instance.transform.localScale;
+        _paddlePos = PaddleController.Instance.transform.position;
+        _ballSize = transform.localScale;
+        _ballPos = transform.position;
+
+        if (_ballPos.x + _ballSize.x / 2f >= _paddlePos.x - _paddleSize.x / 2f &&
+            _ballPos.x - _ballSize.x / 2f <= _paddlePos.x + _paddleSize.x / 2f &&
+            _ballPos.y + _ballSize.y / 2f >= _paddlePos.y - _paddleSize.y / 2f &&
+            _ballPos.y - _ballSize.y / 2f <= _paddlePos.y + _paddleSize.y / 2f
+            )
+        {
+            Velocity.y = -Velocity.y;
+            Velocity.x = UnityEngine.Random.Range(-_maxHorizontalSpeed, _maxHorizontalSpeed);
+        }
+
+        if (_ballPos.x + _ballSize.x / 2f > PaddleController.Instance.MaxPosition.transform.position.x ||
+            _ballPos.x - _ballSize.x / 2f < PaddleController.Instance.MinPosition.transform.position.x)
+        {
+            Velocity.x = -Velocity.x;
+        }
+
+
+        if (_ballPos.y + _ballSize.y / 2f > PaddleController.Instance.MaxPosition.transform.position.y)
+        {
+            Velocity.y = -Velocity.y;
+        }
+
+        transform.position += Velocity * Time.deltaTime;
     }
 }
--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
-public class CubeController : MonoBehaviour
+public class PaddleController : MonoBehaviour
 {
-    public Color color;
     public Vector3 StartPosition, StartScale;
     public float Speed;
-    public GameObject MaxPositionX;
-    public GameObject MinPositionX;
+    public GameObject MaxPosition, MinPosition;
+    public static PaddleController Instance;
     void Start()
     {
+        Instance = this;
         transform.position = StartPosition;
         transform.localScale = StartScale;
     }
@@ -18,15 +19,10 @@ public class CubeController : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.LeftArrow) 
-            && transform.position.x - StartScale.x /2f > MinPositionX.transform.position.x)
+            && transform.position.x - StartScale.x /2f > MinPosition.transform.position.x)
             transform.position += Vector3.left * Time.deltaTime * Speed;
         if (Input.GetKey(KeyCode.RightArrow) 
-            && transform.position.x + StartScale.x /2f < MaxPositionX.transform.position.x)
+            && transform.position.x + StartScale.x /2f < MaxPosition.transform.position.x)
             transform.position += Vector3.right * Time.deltaTime * Speed;
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GetComponent<SpriteRenderer>().color = color;
-            Debug.Log("Hello");
-        }
     }
 }
