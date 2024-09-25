cf121cfeb9f10d52ffb2d3863509abc44afb34ef
DallinEPIC <160791928+DallinEPIC@users.noreply.github.com>
2024-09-25 12:49:57 -0600

    Added bricks
    + Added bricks that can get destroyed
    + Added win screen when all bricks are destroyed

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BallBehaviour : MonoBehaviour
@@ -8,7 +9,7 @@ public class BallBehaviour : MonoBehaviour
     [SerializeField] public float _maxHorizontalSpeed;
     private PaddleController _paddle;
 
-    private Vector3 _paddleSize, _paddlePos, _ballSize, _ballPos;
+    private Vector3 _ballSize, _ballPos;
 
     void Start()
     {
@@ -17,20 +18,29 @@ public class BallBehaviour : MonoBehaviour
 
     void Update()
     {
-        _paddleSize = PaddleController.Instance.transform.localScale;
-        _paddlePos = PaddleController.Instance.transform.position;
-        _ballSize = transform.localScale;
+        if (!GameManager.Instance.GameRunning) return;
+
+            _ballSize = transform.localScale;
         _ballPos = transform.position;
 
-        if (_ballPos.x + _ballSize.x / 2f >= _paddlePos.x - _paddleSize.x / 2f &&
-            _ballPos.x - _ballSize.x / 2f <= _paddlePos.x + _paddleSize.x / 2f &&
-            _ballPos.y + _ballSize.y / 2f >= _paddlePos.y - _paddleSize.y / 2f &&
-            _ballPos.y - _ballSize.y / 2f <= _paddlePos.y + _paddleSize.y / 2f
-            )
+        if (CollidesWithGameObject(PaddleController.Instance.gameObject))
         {
             Velocity.y = -Velocity.y;
             Velocity.x = UnityEngine.Random.Range(-_maxHorizontalSpeed, _maxHorizontalSpeed);
         }
+
+        for(int i = 0; i < GameManager.Instance.Bricks.Length; i++)
+        {
+            if (GameManager.Instance.Bricks[i] != null && CollidesWithGameObject(GameManager.Instance.Bricks[i]))
+            {
+                Destroy(GameManager.Instance.Bricks[i]);
+                GameManager.Instance.Bricks[i] = null;
+                GameManager.Instance.CheckForWin();
+                Velocity.y = -Velocity.y;
+                break;
+            }
+        }
+
 
         if (_ballPos.x + _ballSize.x / 2f > PaddleController.Instance.MaxPosition.transform.position.x ||
             _ballPos.x - _ballSize.x / 2f < PaddleController.Instance.MinPosition.transform.position.x)
@@ -50,5 +60,14 @@ public class BallBehaviour : MonoBehaviour
         }
 
         transform.position += Velocity * Time.deltaTime;
+    }
+
+    private bool CollidesWithGameObject(GameObject go)
+    {
+        return
+            _ballPos.x + _ballSize.x / 2f >= go.transform.position.x - go.transform.localScale.x / 2f &&
+            _ballPos.x - _ballSize.x / 2f <= go.transform.position.x + go.transform.localScale.x / 2f &&
+            _ballPos.y + _ballSize.y / 2f >= go.transform.position.y - go.transform.localScale.y / 2f &&
+            _ballPos.y - _ballSize.y / 2f <= go.transform.position.y + go.transform.localScale.y / 2f;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,27 +9,33 @@ public class GameManager : MonoBehaviour
 
     [HideInInspector] public bool GameRunning;
     public GameObject BallPrefab;
+    public GameObject[] Bricks;
+    public Color BrickColor;
+
     public float RespawnTimer;
     [SerializeField] [Range(0,15)] private int _maxRespawns;
     private int _respawnCount;
+    
     [SerializeField] private GameObject _losePanel;
-
+    [SerializeField] private GameObject _winPanel;
     [SerializeField] private TextMeshProUGUI _ballText;
 
     // Start is called before the first frame update
     void Start()
     {
-        _losePanel.SetActive(false); 
+        _losePanel.SetActive(false);
+        _winPanel.SetActive(false);
         GameRunning = true;
         Instantiate(BallPrefab);
         Instance = this;
         UpdateBallText();
+        RandomizeBrickColors();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey(KeyCode.Space)) RandomizeBrickColors();
     }
 
     public void BallOutOfBounds(GameObject ball)
@@ -55,5 +61,24 @@ public class GameManager : MonoBehaviour
     private void UpdateBallText()
     {
         _ballText.text = "Balls Remaining: " + (_maxRespawns - _respawnCount) + "/" + _maxRespawns;
+    }
+
+    public void CheckForWin()
+    {
+        foreach(GameObject brick in Bricks)
+        {
+            if (brick != null) return;
+        }
+
+        GameRunning = false;
+        _winPanel.SetActive(true);
+    }
+
+    public void RandomizeBrickColors()
+    {
+        foreach (GameObject brick in Bricks)
+        {
+            brick.GetComponent<SpriteRenderer>().color = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value, 1);
+        }
     }
 }
